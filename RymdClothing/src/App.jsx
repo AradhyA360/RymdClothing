4e37c812ade8a07bdf0e609e2fdbabef92a37d62
AradhyA360 <aradhyaaa@hotmail.com>
2024-01-20 17:43:18 +0530

    first commit

---
+++ RymdClothing/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    // <Router>
+    //   <Routes>
+    //     <Route path='/' element={<Home/>} />
+    //     <Route path="/order" element={<Order/>} />
+    //     <Route path="/cart" element={<Cart/>} />
+    //     <Route path="/dashboard" element={<Dashboard/>} />
+    //     <Route path="/nopage" element={<NoPage/>} />
+    //   </Routes>
+    // </Router>
+    <p>hello</p>
+  );
+}
+
+export default App;
