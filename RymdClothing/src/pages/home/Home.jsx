360038c0407d70ec48291e94493cf30a161a4478
AradhyA360 <aradhyaaa@hotmail.com>
2024-01-21 17:51:17 +0530

    Filter Done

--- RymdClothing/src/pages/home/Home.jsx
+++ RymdClothing/src/pages/home/Home.jsx
@@ -2,11 +2,13 @@ import React, { useContext } from "react";
 import Layout from "../../components/layout/Layout";
 import myContext from "../../context/data/myContext";
 import HeroSection from "../../components/herosection/Herosection";
+import Filter from "../../components/filter/Filter";
 
 function Home() {
   return (
     <Layout>
       <HeroSection />
+      <Filter />
     </Layout>
   );
 }
