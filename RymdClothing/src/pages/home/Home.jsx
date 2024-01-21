6d19ce6c65442b6e3b8e9b9483eda259d0a65912
AradhyA360 <aradhyaaa@hotmail.com>
2024-01-21 17:47:02 +0530

    HeroSection Done

--- RymdClothing/src/pages/home/Home.jsx
+++ RymdClothing/src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import Layout from "../../components/layout/Layout";
 import myContext from "../../context/data/myContext";
+import HeroSection from "../../components/herosection/Herosection";
 
 function Home() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <HeroSection />
     </Layout>
   );
 }
