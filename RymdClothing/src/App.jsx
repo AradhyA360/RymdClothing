d222c590974a1b12be48e932c78802465ea5d58f
AradhyA360 <aradhyaaa@hotmail.com>
2024-01-21 19:51:50 +0530

    ProductInfo Done

--- RymdClothing/src/App.jsx
+++ RymdClothing/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Order from "./pages/order/Order";
@@ -7,6 +8,7 @@ import NoPage from "./pages/nopage/NoPage";
 import MyState from "./context/data/myState";
 import Login from "./pages/registration/Login";
 import Signup from "./pages/registration/Signup";
+import ProductInfo from "./pages/productInfo/ProductInfo";
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/productinfo/:id" element={<ProductInfo />} />
           <Route path="/*" element={<NoPage />} />
         </Routes>
       </Router>
