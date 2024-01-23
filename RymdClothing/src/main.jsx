120d8d465236db0ce8888db3568e0a5ca325a6da
AradhyA360 <aradhyaaa@hotmail.com>
2024-01-23 07:36:56 +0530

    Redux Toolkit

--- RymdClothing/src/main.jsx
+++ RymdClothing/src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
+import { store } from "./redux/store.jsx";
+import { Provider } from "./redux/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
